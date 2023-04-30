ca44e159a81e40f99e712bfa45e1578c57cdbaab
angyts <punggolzenith@gmail.com>
2023-04-30 22:36:07 +0800

    backend completed

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 const balances = {
-  "03cc87352ea592eecf86b58c712a00c54eb7fe4afaac3897b26f6f15e9d2a18451": 100,
+  "03068d0835777738cde881a4e0e2533f4a300fa97c348a32bb4b2aabb2ef038759": 100,
   "02b29c2bd4126a809c0d642fbb2785cc1df225a70f94f5a1d126e1723f76d7e7f9": 50,
   "02e3e3ffb9f95dfc0847c1eacde9d09d1cd2da01ef3e17bfcb4997bc13ee78def8": 75,
 };
@@ -19,6 +19,7 @@ app.get("/balance/:address", (req, res) => {
   res.send({ balance });
 });
 
+/* The old function
 app.post("/send", (req, res) => {
   const { sender, recipient, amount } = req.body;
 
@@ -32,6 +33,33 @@ app.post("/send", (req, res) => {
     balances[recipient] += amount;
     res.send({ balance: balances[sender] });
   }
+});
+*/
+
+app.post("/send", (req, res) => {
+  const { r, s, message, sender, recipient, amount } = req.body;
+  //console.log(r, s, message, sender, recipient, amount );
+  console.log(typeof r);
+  let messageHash = helpers.hashMessage(message);
+  let isValid = false;
+  try {
+    isValid = helpers.verifySignature( BigInt(r.slice(0, -1)), BigInt(s.slice(0, -1)), messageHash, sender);
+     }
+     catch {
+     }
+  if (isValid) {
+    setInitialBalance(sender);
+    setInitialBalance(recipient);
+
+    if (balances[sender] < amount) {
+      res.status(400).send({ message: "Not enough funds!" });
+    } else {
+      balances[sender] -= amount;
+      balances[recipient] += amount;
+      res.send({ balance: balances[sender] });
+    }
+  }
+  res.status(400).send({ message: "Signature invalid!" });
 });
 
 app.listen(port, () => {
--- server/scripts/helpers.js
+++ server/scripts/helpers.js
@@ -2,7 +2,8 @@ const secp = require("ethereum-cryptography/secp256k1"); // for crypto
 const { toHex, utf8ToBytes } = require("ethereum-cryptography/utils"); // get hexa from string
 const { keccak256 } = require("ethereum-cryptography/keccak"); // for hash
 
-function verifySignature(signature, messageHash, publicKey) {
+function verifySignature(r,s, messageHash, publicKey) {
+    let signature = new secp.secp256k1.Signature(r, s);
     return secp.secp256k1.verify(signature, messageHash, publicKey);
 }
 
@@ -24,10 +25,18 @@ console.log("Public Key:", publicKey);
 const signature = secp.secp256k1.sign(messageHash, privateKey);
 console.log("Signature:", signature);
 
+//what is the datatype?
+console.log("r is a:", typeof signature.r);
+
+// console.log("Signature compact:", signature.toCompactHex());
+
 // Lets check if it really was signed 
 
-const isValid = verifySignature(signature, messageHash, publicKey);
+const isValid = verifySignature(signature.r, signature.s, messageHash, publicKey);
 console.log("Signature is valid", isValid);
+
+// Lets verify this new signature too
+console.log("New Signature is valid:", verifySignature(97057625583338849092025000485687431676374160955800005300071587767035996497962n, 3114868452147820883301826813245039803837111009248762343699541193970835181103n, messageHash, '03068d0835777738cde881a4e0e2533f4a300fa97c348a32bb4b2aabb2ef038759'));
 
 // Export for use
 module.exports = {
@@ -52,5 +61,13 @@ Account C
 Private Key: 4117b55083c252a8a48dbf97318b9b2faaa4acae7994e7cf5b90d54c7e626f00
 Public Key: 02e3e3ffb9f95dfc0847c1eacde9d09d1cd2da01ef3e17bfcb4997bc13ee78def8
 Valid signature: 
+
+Private Key: 9e93a9be48095ff4a17bcb8fe25277364707ea3b9f90fceed0ade4786cbfda34
+Public Key: 03068d0835777738cde881a4e0e2533f4a300fa97c348a32bb4b2aabb2ef038759
+Signature: Signature {
+  r: 97057625583338849092025000485687431676374160955800005300071587767035996497962n,
+  s: 3114868452147820883301826813245039803837111009248762343699541193970835181103n,
+  recovery: 0
+}
 
 */
